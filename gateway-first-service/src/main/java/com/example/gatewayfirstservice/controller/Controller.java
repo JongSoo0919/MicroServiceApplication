23b07994e438df8fdc3e60d22e387165d8864efc
KimJongSoo <prjs3643@gmail.com>
2024-02-05 07:01:58 +0900

    Spring cloud gateway filter Java Class로 적용

--- gateway-first-service/src/main/java/com/example/gatewayfirstservice/controller/Controller.java
+++ gateway-first-service/src/main/java/com/example/gatewayfirstservice/controller/Controller.java
@@ -1,14 +1,23 @@
 package com.example.gatewayfirstservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/first-service")
 @RestController
+@Slf4j
 public class Controller {
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to My FirstService Application";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service";
     }
 }
--- gateway-second-service/src/main/java/com/example/gatewaysecondservice/controller/Controller.java
+++ gateway-second-service/src/main/java/com/example/gatewaysecondservice/controller/Controller.java
@@ -1,14 +1,23 @@
 package com.example.gatewaysecondservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/second-service")
 @RestController
+@Slf4j
 public class Controller {
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to My Second Application";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service";
     }
 }
