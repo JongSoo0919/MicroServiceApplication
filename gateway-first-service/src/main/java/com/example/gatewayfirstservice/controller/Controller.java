fda65f589549d6720904271d127cf647b96e823f
KimJongSoo <prjs3643@gmail.com>
2024-02-13 06:53:25 +0900

    eureka <-> gateway LB처리

--- gateway-first-service/src/main/java/com/example/gatewayfirstservice/controller/Controller.java
+++ gateway-first-service/src/main/java/com/example/gatewayfirstservice/controller/Controller.java
@@ -1,6 +1,10 @@
 package com.example.gatewayfirstservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class Controller {
+    private final Environment env;
+
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to My FirstService Application";
@@ -22,7 +29,8 @@ public class Controller {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hello World in First Service";
+    public String check(HttpServletRequest request) {
+        log.info("first service server port : ${}",request.getLocalPort());
+        return "Hello World in First Service"+env.getProperty("local.server.port");
     }
 }
--- gateway-second-service/src/main/java/com/example/gatewaysecondservice/controller/Controller.java
+++ gateway-second-service/src/main/java/com/example/gatewaysecondservice/controller/Controller.java
@@ -1,6 +1,10 @@
 package com.example.gatewaysecondservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/second-service")
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class Controller {
+    private final Environment env;
+
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to My Second Application";
@@ -22,7 +29,8 @@ public class Controller {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hello World in Second Service";
+    public String check(HttpServletRequest request) {
+        log.info("my second service port : {}",request.getServerPort());
+        return "Hello World in Second Service" + env.getProperty("local.server.port");
     }
 }
